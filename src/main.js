c9293072c6eaf59e0cd670cce60de9191a086fc7
Nemanja Soc <nemanjasoc@gmail.com>
2019-03-24 16:12:56 +0100

    Init Detail Grid

--- src/main.js
+++ src/main.js
@@ -5,12 +5,15 @@ import { routes } from './routes';
 
 import '@progress/kendo-ui'
 import '@progress/kendo-theme-default/dist/all.css'
+import { Grid } from '@progress/kendo-vue-grid'
 
 new Vue({
    el: '#app' //id of the container
 })
 
 Vue.use(VueRouter);
+
+Vue.component('kendo-grid', Grid);
 
 const router = new VueRouter({
 	routes,
